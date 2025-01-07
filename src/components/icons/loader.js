60ffe2020638845568389d9db70d28d409444975
Aditya1634 <adityadhage66927@gmail.com>
2025-01-07 09:56:02 +0530

    modified frontend

---
+++ src/components/icons/loader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const IconLoader = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="A" transform="translate(31.000000, 27.000000) scale(0.6)">
+        <path
+          d="M9.31818 70H0.454546L26.0909 0.181816H34.8182L60.4545 70H51.5909L30.7273 11.2273H30.1818L9.31818 70ZM12.5909 42.7273H48.3182V50.2273H12.5909V42.7273Z"
+          fill="currentColor"
+        />
+      </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
+    </g>
+</svg>
+);
+
+export default IconLoader;
---
+++ src/components/icons/logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <path
+          d="M30.5 61H26L38.5 30.1H43L55.5 61H51L40.5 37.2H40.2L30.5 61ZM32.3 47.7H49.2V51.7H32.3V47.7Z"
+          fill="currentColor"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
